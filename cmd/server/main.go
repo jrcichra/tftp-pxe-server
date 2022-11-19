6c6ca7d350c98d56f4b69ff70aa7d8d498529d61
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2022-11-19 00:23:26 -0500

    add metrics

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,23 +1,77 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/jrcichra/tftp-pxe-server/internal/server"
+	"github.com/oklog/run"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	directory := flag.String("directory", ".", "directory to serve")
 	port := flag.Int("port", 69, "tftp port")
+	metricsPort := flag.Int("metrics-port", 9101, "metrics port")
 	timeout := flag.Int("timeout", 60, "seconds for tftp timeouts")
 	ipPaths := flag.Bool("ipPaths", true, "prepend request paths with src IP address")
 	flag.Parse()
-	s := server.Server{
-		Directory: *directory,
-		Port:      *port,
-		Timeout:   time.Duration(*timeout) * time.Second,
-		IPPaths:   *ipPaths,
+
+	var g run.Group
+
+	// wait for signal
+	{
+		ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+		defer cancel()
+		g.Add(func() error {
+			<-ctx.Done()
+			cancel()
+			return ctx.Err()
+		}, func(err error) {
+			log.Println(err)
+		})
 	}
-	s.Run()
+
+	// server
+	{
+		s := server.Server{
+			Directory: *directory,
+			Port:      *port,
+			Timeout:   time.Duration(*timeout) * time.Second,
+			IPPaths:   *ipPaths,
+		}
+		prometheus.MustRegister(&s)
+		g.Add(func() error {
+			return s.Run()
+		}, func(err error) {
+			s.Stop()
+		})
+	}
+
+	// metrics
+	{
+		http.Handle("/metrics", promhttp.Handler())
+		ln, err := net.Listen("tcp", fmt.Sprintf(":%d", *metricsPort))
+		if err != nil {
+			panic(err)
+		}
+		g.Add(func() error {
+			return http.Serve(ln, nil)
+		}, func(err error) {
+			ln.Close()
+		})
+	}
+
+	if err := g.Run(); err != nil {
+		log.Println(err)
+	}
+
 }
--- internal/server/tftp.go
+++ internal/server/tftp.go
@@ -5,36 +5,70 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"sync"
 	"time"
 
 	"github.com/pin/tftp"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
-type Server struct {
-	Directory string
-	Port      int
-	Timeout   time.Duration
-	IPPaths   bool
-	server    *tftp.Server
+var (
+	labels              = []string{"ip", "path", "success"}
+	tftpFileRequestDesc = prometheus.NewDesc("tftp_file_request", "How many times a particular file was requested", labels, nil)
+)
+
+type entry struct {
+	ip      string
+	path    string
+	success bool
 }
 
-func (s *Server) Run() {
-	s.server = tftp.NewServer(s.readHandler, nil)
-	s.server.SetTimeout(s.Timeout)
-	log.Printf("Serving TFTP reads on port %d...\n", s.Port)
-	err := s.server.ListenAndServe(fmt.Sprintf(":%d", s.Port))
-	if err != nil {
-		log.Printf("server: %v\n", err)
-		os.Exit(1)
+type Server struct {
+	Directory      string
+	Port           int
+	Timeout        time.Duration
+	IPPaths        bool
+	server         *tftp.Server
+	metricsEntries map[entry]float64
+	metricsLock    sync.RWMutex
+}
+
+func (s *Server) Collect(ch chan<- prometheus.Metric) {
+	s.metricsLock.RLock()
+	defer s.metricsLock.RUnlock()
+	for entry, count := range s.metricsEntries {
+		ch <- prometheus.MustNewConstMetric(tftpFileRequestDesc, prometheus.CounterValue, count, entry.ip, entry.path, strconv.FormatBool(entry.success))
 	}
 }
 
-func (s *Server) readHandler(filename string, rf io.ReaderFrom) error {
+func (s *Server) Describe(desc chan<- *prometheus.Desc) {
+	desc <- tftpFileRequestDesc
+}
+
+var _ prometheus.Collector = &Server{}
+
+func (s *Server) Run() error {
+	s.metricsLock = sync.RWMutex{}
+	s.metricsEntries = make(map[entry]float64)
+	s.server = tftp.NewServer(s.readHandler, nil)
+	s.server.SetTimeout(s.Timeout)
+	log.Printf("Serving TFTP reads on port %d...\n", s.Port)
+	return s.server.ListenAndServe(fmt.Sprintf(":%d", s.Port))
+}
+
+func (s *Server) Stop() {
+	if s.server != nil {
+		s.server.Shutdown()
+	}
+}
+
+func (s *Server) read(filename string, rf io.ReaderFrom) (string, string, error) {
+	// get the remote's IP address
+	ip := rf.(tftp.OutgoingTransfer).RemoteAddr().IP.String()
 	var path string
 	if s.IPPaths {
-		// get the remote's IP address
-		ip := rf.(tftp.OutgoingTransfer).RemoteAddr().IP
-		path = fmt.Sprintf("%s%c%s%c%s", s.Directory, os.PathSeparator, ip.String(), os.PathSeparator, filename)
+		path = fmt.Sprintf("%s%c%s%c%s", s.Directory, os.PathSeparator, ip, os.PathSeparator, filename)
 	} else {
 		path = fmt.Sprintf("%s%c%s", s.Directory, os.PathSeparator, filename)
 	}
@@ -43,13 +77,26 @@ func (s *Server) readHandler(filename string, rf io.ReaderFrom) error {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Printf("%v\n", err)
-		return err
+		return ip, path, err
 	}
 	n, err := rf.ReadFrom(file)
 	if err != nil {
 		log.Printf("%v\n", err)
-		return err
+		return ip, path, err
 	}
 	log.Printf("%d bytes sent for %s\n", n, path)
-	return nil
+	return ip, path, nil
+}
+
+func (s *Server) readHandler(filename string, rf io.ReaderFrom) error {
+	// read, then update metrics
+	ip, path, err := s.read(filename, rf)
+	s.metricsLock.Lock()
+	s.metricsEntries[entry{
+		ip:      ip,
+		path:    path,
+		success: err == nil,
+	}] += 1
+	s.metricsLock.Unlock()
+	return err
 }
